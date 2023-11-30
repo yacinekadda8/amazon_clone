e41e364e15e2f804a47c00de9e8b5689cb8126fd
Merahi yacine <yacinekadda8@gmail.com>
2023-11-30 09:36:03 +0100

    rm error

--- server/middlewares/auth.js
+++
@@ -1,8 +0,0 @@
-# Remove the files from the Git history
-git rm -r --cached server/node_modules
-
-# Update the Git index
-git add .
-
-# Commit the changes
-git commit -m "Remove node_modules from Git history"
